ce72e7da6b1cc5c3bd949fc39580f76f4a53b063
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-03-13 20:53:14 +0300

    Veri yapılarına giriş ve hızlı bir özet geçildi

---
+++ data_structures.py
@@ -0,0 +1,44 @@
+#  Veri yapılarına giriş ve Hızlı özet
+
+#  Sayılar : integer
+y = 5
+type(y)
+
+#  Sayılar : float
+x=10.3
+type(x)
+
+#  Sayılar : complex
+z= 4j + 1
+type(z)
+
+#  String
+x = "Hello ai era"
+type(x)
+
+#  Boolean
+True
+False
+type(True)
+5 == 5
+5 ==100
+type(5==100)
+
+#  Liste
+x = ["btc", "eth", "xrp"]
+type(x)
+
+#  Sözlük (Dictionary)
+x = {"name":"Ceren", "Age": 22}
+type(x)
+
+#  Tuple (Demet)
+x = ("python", "ml", "ds")
+type(x)
+
+#  Set (Kümeler olarak düşünülebilir)
+x ={"python", "ml", "ds"}
+type(x)
+#  Sözlüklerden tek farkı ,key- value durumu, iki nokta kullanımı yoktur.
+
+#  Not: Liste, tuple, set ve dictionary veri yapıları aynı zamanda Python Collections(Arrays) olarak geçmektedir.
