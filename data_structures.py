ac1b129d739392330a577cefc734a0d2af7409cf
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-03-15 23:51:09 +0300

    Set' ler ve önemli metodlar hakkında bilgiler verildi.

--- data_structures.py
+++ data_structures.py
@@ -255,3 +255,77 @@ dictionary.items()
 
 dictionary.update({"RF": 10})
 dictionary
+
+
+#  Demet (Tuple)
+#  Değiştirilemez
+#  Sıralıdır.
+#  Kapsayıcıdır.
+#  Kullanım sıklığı çok düşüktür.
+t = ("john", "mark", 1, 2)
+type(t)
+
+t[0]
+t[0:3]
+#  Tuple elemanlarında herhangi bir değişiklik yapılmaz tuple-ı bir listeye çeviriseniz elemanlarını değiştirebilirsiniz
+
+t = list(t)
+t[0] = 99
+t = tuple(t)
+t
+
+#  Set (Kümede denilebilir.)
+
+#  Değiştirilebilir
+#  Sırasız+ eşsizdir
+#  Kapsayıcıdır.
+
+#  difference(): İki kümenin Farkı
+
+set1 = set([1, 3, 5])
+set2 = set([1, 2, 3])
+#  set1 de olup set2 de olmayanlar
+
+set1.difference(set2)
+set1 - set2
+#  set 2 de olup set1 de olmayanlar
+
+set2.difference(set1)
+set2 - set1
+#  symetric_difference(): İki kümede de birbirlerine göre olmayanlar
+
+set1.symmetric_difference(set2)
+set2.symmetric_difference(set1)
+
+#  intersection(): İki kümenin kesişimi
+
+set1 = set([1, 3, 5])
+set2 = set([1, 2, 3])
+set1 & set2
+
+set1.intersection(set2)
+set2.intersection(set1)
+
+#  union(): İki kümenin birleşimi
+
+set1.union(set2)
+set2.union(set1)
+
+#  isdisjoint(): İki kümenin kesişimi boş mu?
+
+set1 = set([7, 8, 9])
+set2 = set([5, 6, 7, 8, 9, 10])
+
+set1.isdisjoint(set2)
+set2.isdisjoint(set1)
+
+#  issubset(): Bir küme diğer kümenin alt kümesi mi?
+
+set1.issubset(set2)
+set2.issubset(set1)
+set1.issubset(set1)
+
+#  issuperset(): Bir küme diğer kümeyi kapsıyor mu?
+
+set2.issuperset(set1)
+set1.issuperset(set2)
